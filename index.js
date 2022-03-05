4a60dff21fe6bf55bf2849e111892ea8aea9d283
Yvan <i@zhuyunfeng.com>
2022-03-05 17:46:00 +0800

    :tada: 0.0.4

--- index.js
+++ index.js
@@ -1,39 +1,45 @@
 const data = {
   text: 123,
 };
-let activeEffect = null
+let activeEffect = null;
 function effect(fn) {
-  activeEffect = fn
-  activeEffect()
+  activeEffect = fn;
+  activeEffect();
 }
-const bucket = new WeakMap;
+const bucket = new WeakMap();
 const obj = new Proxy(data, {
   get(target, key) {
-    let depsMap = bucket.get(target)
-    if(!depsMap) {
-      bucket.set(target, (depsMap = new Map()))
-    }
-    let deps = depsMap.get(key)
-    if(!deps) {
-      depsMap.set(key, (deps = new Set()))
-    }
-    deps.add(activeEffect) 
+    track(target, key)
     return target[key];
   },
   set(target, key, value) {
     target[key] = value;
-    let depsMap = bucket.get(target)
-    if(!depsMap) return
-    let deps = depsMap.get(key)
-    deps && deps.forEach((fn) => fn());
-    return true
+    trigger(target, key)
+    // return true;
   },
 });
 
+function track(target, key) {
+  let depsMap = bucket.get(target);
+  if (!depsMap) {
+    bucket.set(target, (depsMap = new Map()));
+  }
+  let deps = depsMap.get(key);
+  if (!deps) {
+    depsMap.set(key, (deps = new Set()));
+  }
+  deps.add(activeEffect);
+}
 
+function trigger(target, key) {
+  let depsMap = bucket.get(target);
+  if (!depsMap) return;
+  let deps = depsMap.get(key);
+  deps && deps.forEach((fn) => fn());
+}
 
 effect(function () {
-  console.log('effect run');
+  console.log("effect run");
   document.body.innerText = obj.text;
 });
 
@@ -42,12 +48,12 @@ setTimeout(() => {
 }, 1000);
 
 setTimeout(() => {
-  obj.noExist = 789
+  obj.noExist = 789;
 }, 2000);
 setTimeout(() => {
-  obj.noExist = 389
+  obj.noExist = 389;
 }, 3000);
 
 setTimeout(() => {
-  obj.noExist = 389
+  obj.noExist = 389;
 }, 4000);
